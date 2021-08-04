49edceb3d54e8a746c7322ea5c5b55914fe16bea
Ben De Loyola <bendeloyola2017@gmail.com>
2021-08-04 20:59:00 +0800

    change spaces 2

--- lesson_1/class.rb
+++ lesson_1/class.rb
@@ -1,13 +1,13 @@
 class Animal 
-    def initialize(name, color, age)
-        @name = name
-        @color = color
-        @age = age
-    end
-    def walk
-        puts "hey! #{@name}, lets walk"
-        puts "#{@name} is #{@color}"
-    end
+  def initialize(name, color, age)
+    @name = name
+    @color = color
+    @age = age
+  end
+  def walk
+    puts "hey! #{@name}, lets walk"
+    puts "#{@name} is #{@color}"
+  end
 end
 
 dog = Animal.new("boni", "white", 5)
