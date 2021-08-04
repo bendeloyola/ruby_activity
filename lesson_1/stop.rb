4cb9da1177dbc5343ecc5b6243553164e7ae4f71
Ben De Loyola <bendeloyola2017@gmail.com>
2021-08-04 21:11:21 +0800

    fix spaces

--- lesson_1/stop.rb
+++ lesson_1/stop.rb
@@ -1,7 +1,7 @@
 w = ""
 while w != "STOP" do
-    puts "Hi, How are you feeling?"
-    ans = gets.chomp
-    puts "Want me to ask you again?"
-    w = gets.chomp
+  puts "Hi, How are you feeling?"
+  ans = gets.chomp
+  puts "Want me to ask you again?"
+  w = gets.chomp
 end
