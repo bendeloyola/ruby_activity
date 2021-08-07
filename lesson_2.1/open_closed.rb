28c27ef5f8d32ea84c01871610a3d1564a53d05f
Ben De Loyola <bendeloyola2017@gmail.com>
2021-08-07 12:22:55 +0800

    open_closed sample

--- lesson_2.1/open_closed.rb
+++ lesson_2.1/open_closed.rb
@@ -1,9 +1,27 @@
-class Report
-  def body
-    generate_report_sample
-  end
-
-  def print
-    print_report
+class NotificationSender
+  def send(user, message, sender = EmailSender.new)
+    sender.send(user, message) if user.active?
   end
 end
+
+class Sender
+  def send(user:, message:)
+    raise NotImplementedError
+  end
+end
+
+class EmailSender < Sender
+  def send(user:, message:)
+    # implementation for Email
+  end
+end
+
+class SmsSender < Sender
+  def send(user:, message:)
+    # implementation for SMS
+  end
+end
+
+sender = NotificationSender.new
+sender.send(user, "Hello World", SmsSender.new)
+sender.send(user, "Hello World", EmailSender.new)
