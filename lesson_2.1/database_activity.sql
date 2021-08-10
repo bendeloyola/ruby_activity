399b69b3f1e72c98af10395098c5c4685ee3c366
Ben De Loyola <bendeloyola2017@gmail.com>
2021-08-10 20:07:30 +0800

    database activity 2

--- lesson_2.1/database_activity.sql
+++ lesson_2.1/database_activity.sql
@@ -1,15 +1,23 @@
-CREATE TABLE STUDENT (id INT NOT NULL PRIMARY KEY, first_name varchar(255) NOT NULL, middle_name varchar(255) NOT NULL, last_name varchar(255) NOT NULL, age integer, location varchar(255) NOT NULL);
+CREATE TABLE STUDENT (id INT NOT NULL PRIMARY KEY, first_name varchar(255) NOT NULL, middle_name varchar(255), last_name varchar(255) NOT NULL, age integer, location varchar(255) NOT NULL);
 
 
 INSERT INTO STUDENT(id, first_name, middle_name, last_name, age, location)
-VALUES 	(1, "ben", "matillano", "de loyola", 26, "quezon city"),
-	(2, "jen", "mandap", "badillo", 26, "pasig"),
-	(3, "john", "cruz", "carlos", 22, "makati"),
-	(4, "danes", "gordo", "richard", 15, "montalban"),
-	(5,  "justin", "milda", "dela cuz", 26, "pasay"),
-	(6, "byron", "jolo", "predas", 28, "pangasinan");
+VALUES 	(1, "jJuan", "", "Cruz", 18, "Manila"),
+	(2, "Anne", "", "Wall", 20, "Manila"),
+	(3, "Theresa", "", "Joseph", 21, "Manila"),
+	(4, "Isaac", "", "Gray", 19, "Laguna"),
+	(5,  "Zack", "", "Matthews", 22, "Marikina"),
+	(6, "Finn", "", "Lam", 25, "Manila");
 
 
 UPDATE STUDENT SET first_name="Ivan", middle_name="Ingram", last_name="Howard", age=25, location="Bulacan"  WHERE id=1;
 
 DELETE FROM STUDENT WHERE id=6;
+
+SELECT COUNT(id) FROM STUDENT;
+
+SELECT * FROM STUDENT WHERE location = "Manila";
+
+SELECT AVG(age) FROM STUDENT;
+
+SELECT * FROM STUDENT ORDER BY age DESC;
